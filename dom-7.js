1d35c2689617e3fed75522dbaf73c02f84f448db
Mohith <mohithog@gmail.com>
2024-09-10 20:39:10 +0530

    Question no:7 completed

---
+++ dom-7.js
@@ -0,0 +1,14 @@
+/**Display a progress bar that updates in real-time, showing the progress of a task ,
+download , or form submisiion.           */
+var prg = document.querySelector("#progress");
+var h3 = document.querySelector("h3");
+
+var count = 0;
+var int = setInterval(function(){
+    if(count===100){
+        h3.style.opacity=1;
+        clearInterval(int);
+    }
+    count++;
+    prg.style.width = count+'%';
+},100)
